9134d4ebf900d461c128a371e9c27ca1ae141d36
Ferochoa5 <ferochoa262@gmail.com>
2025-03-30 20:27:03 -0600

    cambio de texto en inicio

--- FREYJA/App.js
+++ FREYJA/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hola a todos</Text>
+      <Text>hola a todossss</Text>
       <StatusBar style="auto" />
     </View>
   );
